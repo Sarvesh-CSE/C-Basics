3f01c2599b54c3a5c4f95a73602543013bed2f6a
Sarvesh <pandeysarvesh100@gmail.com>
2020-12-08 12:01:52 +0530

    arithmatic ops for integers

---
+++ sum_int_data.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num1, num2, res;
+    printf("Enter any two number: ");
+    scanf("%d%d", &num1, &num2);
+    res = num1+num2;
+    printf("\nAddition = %d", res);
+    res = num1-num2;
+    printf("\nSubtraction = %d", res);
+    res = num1*num2;
+    printf("\nMultiplication = %d", res);
+    res = num1/num2;
+    printf("\nDivision = %d", res);
+    getch();
+    return (1);
+}
