144731b1456bad48d0e8edd7addf47e02994e0e3
Sarvesh <pandeysarvesh100@gmail.com>
2020-12-18 09:51:12 +0530

    if ... else if ... else usecase

---
+++ ConditionalConstruct/Implementation/if_elseif_else_usecase.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int num = 5;
+    if (num == 1)
+        printf("Number is one");
+    else if (num == 2)
+    {
+        printf("Number is two");
+    }
+    else if (num == 3)
+    {
+        printf("Number is three");
+    }
+    else
+    {
+        printf("Number entered is unknown to us");
+    }
+    
+    
+    
+}
