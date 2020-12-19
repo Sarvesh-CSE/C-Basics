a27f2f3c865ed29f44b60d5faaee2a7ffc6e00b8
Anjali Kumari <75776262+Anjali-1801@users.noreply.github.com>
2020-12-19 09:38:13 +0530

    Update simple_interest.c

--- simple_interest.c
+++ simple_interest.c
@@ -4,15 +4,10 @@ and the rate is 10%. Also, calculate the total amount at the end of 1 year*/
 #include <stdio.h>
 int main(void)
 {
-    int p,n,A;
-    float r,si;
-    printf("Enter p:Rs.");
-    scanf("%d",&p);
-    printf("Enter n:");
-    scanf("%d",&n);
-    printf("Enter r:");
-    scanf("%f",&r);
-    printf("Simple Interest (si):");
+    int p=2000,n=2,A;
+    float r=10,si;
+    printf("p: %d\nn: %d\nr: %f", p, n, r);
+    printf("\nSimple Interest (si):");
     si=p*n*r/100;
     printf("%f \n",si);
     printf("Total Amount:");
