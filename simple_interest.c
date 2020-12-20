f05db270da9121c2063b2eb254e3e6386dab00d3
Anjali Kumari <75776262+Anjali-1801@users.noreply.github.com>
2020-12-20 20:51:36 +0530

    Changes made
    Both printf and scanf used separately in a single statement for p, n, and r.

--- simple_interest.c
+++ simple_interest.c
@@ -4,13 +4,14 @@ and the rate is 10%. Also, calculate the total amount at the end of 1 year*/
 #include <stdio.h>
 int main(void)
 {
-    int p=2000,n=2,A;
-    float r=10,si;
-    printf("p: %d\nn: %d\nr: %f", p, n, r);
-    printf("\nSimple Interest (si):");
+    int p,n,A;
+    float r,si;
+    printf("Enter p, n, r: ");
+    scanf ("%d %d %f", &p, &n, &r);
+    printf("Simple Interest (si): ");
     si=p*n*r/100;
     printf("%f \n",si);
-    printf("Total Amount:");
+    printf("Total Amount: ");
     A=p+si;
     printf("%d",A);
     return 0;
