00a138bad7573fc410191387d210b7bcdc5fa478
Ishita-Singhal <ishitasinghal202@gmail.com>
2020-12-21 13:22:31 +0530

    Reduced the number of lines and increased the interval of ranges.

--- ConditionalConstruct/Implementation/else_if_ladder_using_scanf.c
+++ ConditionalConstruct/Implementation/else_if_ladder_using_scanf.c
@@ -8,25 +8,12 @@ void main()
     scanf("%d",&number);
     printf("\n\n");
 
-    if(number>90)
-        printf("Your inputed number is in the range of 91-100");
-    else if(number>80)
-        printf("Your inputed number is in the range of 81-90");
-    else if(number>70)
-        printf("Your inputed number is in the range of 71-80");
-    else if(number>60)
-        printf("Your inputed number is in the range of 61-70");
-    else if(number>50)
-        printf("Your inputed number is in the range of 51-60");
-    else if(number>40)
-        printf("Your inputed number is in the range of 41-50");
-    else if(number>30)
-        printf("Your inputed number is in the range of 31-40");
-    else if(number>20)
-        printf("Your inputed number is in the range of 21-30");
-    else if(number>10)
-        printf("Your inputed number is in the range of 11-20");
+    if(number>76)
+        printf("Your inputed number is in the range of 76-100");
+    else if(number>51)
+        printf("Your inputed number is in the range of 51-75");
+    else if(number>26)
+        printf("Your inputed number is in the range of 26-50");
     else 
-        printf("Your inputed number is in the range of 1-10");                           
-           
+        printf("Your inputed number is in the range of 1-25");  
 }
