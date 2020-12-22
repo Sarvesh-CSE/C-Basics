51fab794757f344bf80d2f96ad31d8e2d0fc7a8c
Vaishnavi-iit <75825446+Vaishnavi-iit@users.noreply.github.com>
2020-12-22 17:39:16 +0530

    Update ternary_op_usecase.c

--- Operators/ternary_op_usecase.c
+++ Operators/ternary_op_usecase.c
@@ -7,3 +7,16 @@ int main()
     printf("y =%d ",y);
     return 0;
 }
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter value of a and b");
+    scanf("%d,%d",&a,&b);
+    (a%b==0)?(printf("a is multiple of b")):(printf("a is not multiple of b"));
+    return(0);
+}
+                                             
+                                             
+                                                                                                                                  
+                                             
