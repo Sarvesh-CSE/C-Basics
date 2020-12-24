17e84b4ed79c24c507fb558758792ca5c9a2a920
Ritika014 <ritikabailwal99@gmail.com>
2020-12-25 02:39:19 +0530

    Added a program which finds average of n natural numbers.

---
+++ for_loop.c
@@ -0,0 +1,24 @@
+/* FOR LOOP
+C program to calculate average of any n natural numbers entered through the keyboard
+*/
+#include <stdio.h>
+
+int main()
+ {
+  int n, num, i;
+  float sum=0, avg;
+  printf("How many numbers:\n");
+  scanf("%d",&n);
+  printf("Enter the natural numbers:\n");
+
+  for (i= 0; i < n; i++)
+  {
+    scanf("%d", &num);
+    sum += num;
+  }
+
+ avg = sum/n;
+ printf("The average of n natural numbers are: %f", avg);
+  
+  return 0;
+}
