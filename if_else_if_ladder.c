b8b078c2c55cc1df7fe8ba731ae5836023c6ca16
chanchal3899 <chanchalchauhan3899@gmail.com>
2020-12-26 23:24:54 +0530

    first attempt

---
+++ if_else_if_ladder.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int marks;
+    printf("Please enter your marks\n");
+    scanf("%d", &marks);
+
+    if (marks>=90 && marks<100){
+        printf ("Your grade is A\n");
+    }
+    else if (marks>=70 && marks<89){
+        printf ("Your grade is B\n");
+    }
+    else if (marks>=50 && marks<=69){
+        printf ("Your grade is C\n");
+    }
+    else if (marks>=30 && marks<=49){
+        printf ("Your grade is D\n");
+    }
+    else if (marks<30){
+        printf ("Your grade is E\n");
+    }
+    else {
+        printf ("Invalid marks... Please enter the correct marks\n");
+    }
+
+    return 0;
+}
