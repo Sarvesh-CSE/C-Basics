36c403e74d71e62c296678a34cda32272743fd87
Anjali <akumari1897@gmail.com>
2020-12-27 10:08:21 +0530

    Incorporated comma operator

--- Operators/nested_ternary_operator.c
+++ Operators/nested_ternary_operator.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int var= 75;
-    int var2=56;
-    int num;
+    int var= 75, var2= 56, num;
     num= sizeof(var) ? (var2>23 ? ((var==75) ? 'A' : 0) :0) :0;
     printf("%d", num);
     return 0;
