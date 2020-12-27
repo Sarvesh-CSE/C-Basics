f6313ef6c15c5ea6913ec48c2d85e71c2453e2f6
Anjali <akumari1897@gmail.com>
2020-12-27 15:09:18 +0530

    Changes made

--- student_div.c
+++ student_div.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
-Int main()
+int main()
 {
-    Int m1,m2,m3,m4,m5,per;
-    Printf("enter marks in five subjects");
-    Scanf("%d%d%d%d%d",&m1,&m2,&m3,&m4,&m5);
+    int m1,m2,m3,m4,m5;
+    float per;
+    printf("enter marks in five subjects: ");
+    scanf("%d%d%d%d%d",&m1,&m2,&m3,&m4,&m5);
 
-     Per=(m1+m2+m3+m4+m5)/500*100;
+    per=(m1+m2+m3+m4+m5)*100/500; 
+    printf("Percentage: %f\n", per);
+    if(per>=60)
+          printf("first division\n");
      
-    If(per>=60)
-          Printf("first division\n");
-     
-    If((per>=50)&&(per<50))
-          Printf("second division\n");
+    else if((per>=50)&&(per<60))
+          printf("second division\n");
 
-    If(per<40)
-          Printf("fail\n");
+    else if(per<40)
+          printf("fail\n");
    return 0;
 }
