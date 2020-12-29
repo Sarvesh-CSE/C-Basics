6edc3a132dc98c5dadf95bfe5fc60caee155dc83
Vaishnavi-iit <75825446+Vaishnavi-iit@users.noreply.github.com>
2020-12-29 16:41:02 +0530

    Update and rename shruti to shruti.c
    just go through the changes.

--- shruti.c
+++ shruti.c
@@ -4,22 +4,19 @@ int main()
 {
     int age;
     printf("Enter your age\n");
-
     scanf("%d", &age);
     printf("You have entered %d as your age\n", age);
-    if (age>18) {
+    if (age>18)
+    {
       printf(" you can vote");
-
     }
     else if(age>10)
     {
         printf("you can vote for kids.");
     }
-    else{
+    else
+    {
         printf("you cannot vote");
     }
-
     return 0;
-
-
 }
