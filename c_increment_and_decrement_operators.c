5e70e86ae4208432f134dc36452e8b0ad12785ac
Vaishnavi-iit <75825446+Vaishnavi-iit@users.noreply.github.com>
2020-12-29 16:30:23 +0530

    Update and rename c increment and decrement operaters to c_increment_and_decrement_operators.c

--- c_increment_and_decrement_operators.c
+++ c_increment_and_decrement_operators.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main ()
 {
-    int a=10,b =100
-    float c=10.5, d=100.5
+    int a=10,b =100;
+    float c=10.5, d=100.5;
     printf("++a = %d \n",++a);
     printf("--b = %d \n",--b);
     printf("++c = %f \n",++c);
