1ca80b4c3527185306b6a21232eb5c6eba118365
shruti0803 <76167239+shruti0803@users.noreply.github.com>
2020-12-29 20:52:40 +0530

    multiplication.c

---
+++ multiplication.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n;
+    printf("enter the number:\n");
+    scanf("%d", &n);
+    printf("MULTIPLICATION TABLE\n", n);
+    for(int i = 1; i<=10; i++)
+    {
+        printf("%d X %d= %d\n", n, i, n*i);
+    }
+    return 0;
+}
