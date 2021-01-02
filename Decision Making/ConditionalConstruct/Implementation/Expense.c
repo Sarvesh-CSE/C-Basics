9929705493a77c0987a8b02ca41d72ca8369ee2b
Syona Tiwari <74848884+CrescentSurfing@users.noreply.github.com>
2021-01-02 12:32:49 +0530

    Update Expense.c

--- Decision Making/ConditionalConstruct/Implementation/Expense.c
+++ Decision Making/ConditionalConstruct/Implementation/Expense.c
@@ -6,11 +6,13 @@ int main()
     float rate, tot;
     printf("Enter quantity and rate:");
     scanf("%d%f", &qty, &rate);
-   { if (qty>1000)
+    
+    if (qty>1000)
         dis = 10;
     else
-        dis = 0;}
+        dis = 0;
+    
     tot = (qty*rate)-(qty*rate*dis/100);
     printf("Total expenses = Rs. %f\n", tot);
     return 0;
-    }
+}
