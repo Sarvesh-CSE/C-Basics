0d5e640abd75e11b6b5df30a88526b05afe246a5
Syona <syona16t@gmail.com>
2021-01-02 12:11:57 +0530

    Rearranged files

---
+++ Decision Making/ConditionalConstruct/Implementation/Expense.c
@@ -0,0 +1,16 @@
+/*Calculation of Expenses*/
+#include<stdio.h>
+int main()
+{
+    int qty, dis;
+    float rate, tot;
+    printf("Enter quantity and rate:");
+    scanf("%d%f", &qty, &rate);
+   { if (qty>1000)
+        dis = 10;
+    else
+        dis = 0;}
+    tot = (qty*rate)-(qty*rate*dis/100);
+    printf("Total expenses = Rs. %f\n", tot);
+    return 0;
+    }
