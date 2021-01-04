3c6e1cecce299119067699b9e62c90528fa644a4
Sarvesh <pandeysarvesh100@gmail.com>
2021-01-04 16:31:46 +0530

    intelligent use of &

--- Using Array/sum_array.c
+++ Using Array/sum_array.c
@@ -4,7 +4,7 @@ int main()
     int arr[5], sum = 0;
     for(int i = 0; i< 5; i++) {
         printf("Enter the %d element = ", i + 1);
-        scanf("%d", &arr[i]);
+        scanf("%d", &arr[i]); //arr[i]
         sum+=arr[i];
     }
     printf("The sum of the elements is = %d", sum);
