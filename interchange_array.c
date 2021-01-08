f25bfc37b9fd63c1de7f78a3048e95eada406340
Gitika12 <75841184+Gitika12@users.noreply.github.com>
2021-01-08 21:01:44 +0530

    Added given and interchanged array.
    added command to print given array and interchanged array.

--- interchange_array.c
+++ interchange_array.c
@@ -4,14 +4,22 @@ int main()
 {
     int num[]={12, 4, 5, 11, 9, 21, 17, 19, 52, 34};
     int i, t;
-    for(i=0; i<=9; i=i+2)
-    {
+      printf("\nThe given array is:\n");
+       for(i=0; i<=9; i++)
+       {
+            printf("%d\t",num[i]);
+       }
+       for(i=0; i<=9; i=i+2)
+       {
         t=num[i];
         num[i]=num[i+1];
         num[i+1]=t;
-    }
-    for(i=0; i<=9; i++)
+       }
+        printf("\nThe interchanged array is:\n");
+       for(i=0; i<=9; i++)
+       {
         printf("%d\t",num[i]);
+       }  
     return(0);
 }
 
