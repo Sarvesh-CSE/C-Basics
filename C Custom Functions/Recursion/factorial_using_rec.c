39fa5bfa9e9af13def345119159b9d2bb43e8319
Sarvesh <pandeysarvesh100@gmail.com>
2021-01-08 09:56:44 +0530

    update - removed whitespaces

--- C Custom Functions/Recursion/factorial_using_rec.c
+++ C Custom Functions/Recursion/factorial_using_rec.c
@@ -1,20 +1,15 @@
 #include<stdio.h>
-
 int fact(int n)
 {
     if(n <= 1)
         return 1;
     return n * fact(n-1);
 }
-
 int main()
 {
     int n;
-
     scanf("%d",&n);
-
     if(n >= 0)
         printf("%d\n",fact(n));
-
     return 0;
 }
