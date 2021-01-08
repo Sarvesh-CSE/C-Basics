618aa6d68522fb5a5a09909b713da53a7e932520
Anushka <76489794+Anushka0912@users.noreply.github.com>
2021-01-09 02:37:08 +0530

    Create Fibonacci_series.c
    This program prints fibonacci series

---
+++ Fibonacci_series.c
@@ -0,0 +1,27 @@
+  #include<stdio.h>
+  void fibonacci(void);
+  void main() 
+     { 
+   	fibonacci();
+     } 
+   
+   void fibonacci() 
+     {
+   	int arr[50];
+   	arr[0]=0;
+   	arr[1]=1;
+   	printf ("%d", arr[0]);
+   	printf("\t");
+   	printf ("%d", arr[1]);
+   	printf("\t");
+   	
+   	for(int i=0;i<=23;i++)
+   	  { 
+   	        arr[i+2]=arr[i]+arr[i+1];
+   	  	printf ("%d", arr[i+2]);
+   	        printf("\t");
+   	  } 
+   	
+        } 
+   	  	
+   	
