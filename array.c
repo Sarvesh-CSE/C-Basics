2e827d258caeaa28f2d1c71f993b7e68f7b41f7d
shruti0803 <76167239+shruti0803@users.noreply.github.com>
2021-01-10 15:50:02 +0530

    Update array.c

--- array.c
+++ array.c
@@ -5,7 +5,7 @@ int main()
     for(int i = 0; i<4; i++)
     {
     
-    printf("the value  %d element of the array is %d\n",i, marks[i]);
+    printf("The value of  %d element of the array is %d\n",i, marks[i]);
     }
     return 0;
 }
