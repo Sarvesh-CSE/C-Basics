e3ef78b1f24526c4ccd49e888c029953b817cc99
Vaishnavi-iit <75825446+Vaishnavi-iit@users.noreply.github.com>
2021-01-11 12:20:53 +0530

    Create code_for_tower_of_hanoi.c

---
+++ code_for_tower_of_hanoi.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void TOH(int n,char x,char y,char z)                     //Function definition
+{
+        if(n>0)
+    {
+        TOH(n-1,x,z,y);
+        printf("move from %c to %c\n",x,z);              
+        TOH(n-1,y,x,z);                                 //recursion
+    }
+}
+void main()
+{
+        int n;
+        printf("enter the number of disks:",n);
+        scanf("%d",&n);
+        TOH(n,'A','B','C');                            //function call
+}
