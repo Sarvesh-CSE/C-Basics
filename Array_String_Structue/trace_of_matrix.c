47a5d8047ca3f32c90bd1bf21520ce5abb38e2a6
Vaishnavi-iit <75825446+Vaishnavi-iit@users.noreply.github.com>
2021-01-12 09:53:55 +0530

    Create trace_of_matrix.c

---
+++ Array_String_Structue/trace_of_matrix.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#define MAXROW 10
+#define MAXCOL 10
+void main()
+{
+         int r,c,i,j,m[MAXROW][MAXCOL],sum;
+         printf("enter number of row and colume:",r);
+         scanf("%d",&r);
+         for(i=0;i<r;i++)
+         {
+               for(j=0;j<r;j++)
+               {
+                    printf("enter element [%d,%d]:",i+1,j+1);
+                    scanf("%d",&m[i][j]);
+               }
+        }
+        sum=0;
+        for(i=0;i<r;i++)
+        {
+              for(j=0;j<r;j++)
+              {
+                    if(i==j)
+                    sum+=m[i][j];
+              }
+              continue;
+        }
+       printf("trace of matrix: %d",sum);
+}
+
