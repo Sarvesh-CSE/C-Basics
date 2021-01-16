a1f968b9bd79cdf37357850dfb369955a7889915
Vaishnavi-iit <75825446+Vaishnavi-iit@users.noreply.github.com>
2021-01-16 11:30:17 +0530

    Create repeated_number_in_array.c

---
+++ Array_String_Structue/repeated_number_in_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int a[5],i=0,j=0;
+    printf("enter 5 number");
+    for(i=0;i<5;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<5;i++)
+    {
+        for(j=i+1;j<5;j++)
+         {
+            if(a[i]==a[j])
+               printf("repeated number is:%d\n",a[j]);
+         }
+    }
+}
+
