f345bb15b9bb5eaf5d835793350bf1cb22ae2db8
Anjali <akumari1897@gmail.com>
2021-01-31 14:28:55 +0530

    Added comments to the file sum_array_elements_using_pointers.c

--- 1.Practicals_C/Pointers/sum_array_elements_using_pointers.c
+++ 1.Practicals_C/Pointers/sum_array_elements_using_pointers.c
@@ -6,9 +6,9 @@ int main ()
     int arr[] = {2, 3, 4, 5, 6};
     int sum= 0, *p;
 
-    for (p= &arr[0]; p<= &arr[4]; p++)
-         sum += *p;
+    for (p= &arr[0]; p<= &arr[4]; p++)  // assign address of zeroth element to p, start iteration, reach at every array element location (increment pointer to point to next location)
+         sum += *p;                      // access the value at each array element location and add to the sum variable.
 
-    printf ("The sum of the array elements: %d", sum);
+    printf ("The sum of the array elements: %d", sum); //print sum
     return 0;
 }
