7d71a3ccfe0907b263af41fbcad7d84af6d22177
anchal071 <75790418+anchal071@users.noreply.github.com>
2021-02-08 11:28:20 +0530

    Create enumerators.c

---
+++ enumerators.c
@@ -0,0 +1,21 @@
+enum boolean
+{
+ false ,true;
+}
+boolean iseven(int x);
+  return (true);
+else
+  return(false);
+Void main()
+{
+ int n;
+ clrscr();
+ printf("enter a number");
+ scanf(%d,&n);
+ result = iseven(n);
+ if(result==true);
+  printf("even numbers");
+ else
+  printf("odd numbers");
+ getch();
+}
