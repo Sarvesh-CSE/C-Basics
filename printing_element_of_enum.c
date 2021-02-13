a7c927d1b54f2cb6abf1440564fdaca91e3b6be3
Vaishnavi-iit <75825446+Vaishnavi-iit@users.noreply.github.com>
2021-02-13 16:00:26 +0530

    Update printing_element_of_enum.c

--- printing_element_of_enum.c
+++ printing_element_of_enum.c
@@ -7,5 +7,5 @@ void main()
   /* as argument i is passed through printf statement therefore compiler would print values of i as 1,2,3,4,5,6,7,8,9, corresponding to Jan to Sep
   and then goes on printing  next values of i each time incremented by 1 till the value of i becomes 20 which corresponds to  Oct and
   then 21 and 22 corresponding to Nov and Dec.*/
-         printf("%d %d %d %d %d %d %d %d %d %d %d %d",Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec);       
+         printf("%d %d %d %d %d %d %d %d %d %d %d %d",Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec);   //to print the values of months only  
 }
