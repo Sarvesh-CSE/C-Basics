bc8aaced52aba1c575e1f06db814574146f8015d
Vaishnavi-iit <75825446+Vaishnavi-iit@users.noreply.github.com>
2021-02-15 21:55:44 +0530

    Update and rename printing_element_of_enum.c to 1.Practicals_C/Array_String_Structue_Union/printing_elements_of_enum.c

--- 1.Practicals_C/Array_String_Structue_Union/printing_elements_of_enum.c
+++ 1.Practicals_C/Array_String_Structue_Union/printing_elements_of_enum.c
@@ -2,13 +2,17 @@
 enum year{Jan=1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct=20, Nov, Dec};
 void main()
 {   
-    printf("Jan=%d\nDec=%d",Jan,Dec);
-    printf("\n");
+    printf("Jan=%d\nDec=%d\n",Jan,Dec);
     for (int i=Jan;i<=Dec;i++)
-         printf("%d",i);
+         printf("%d ",i);
   /* as argument i is passed through printf statement therefore compiler would print values of i as 1,2,3,4,5,6,7,8,9, corresponding to Jan to Sep
   and then goes on printing  next values of i each time incremented by 1 till the value of i becomes 20 which corresponds to  Oct and
   then 21 and 22 corresponding to Nov and Dec.*/
     printf("\n");     
-    printf("%d %d %d %d %d %d %d %d %d %d %d %d",Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec);   //to print the values of months only  
+    printf("%d %d %d %d %d %d %d %d %d %d %d %d\n",Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec);   //to print the values of months only 
+    for(int j=Jan;j<=Sep;j++)      
+        printf("%d ",j);
+        printf("\n");
+    for(int k=Oct;k<=Dec;k++)
+        printf("%d ",k);
 }
