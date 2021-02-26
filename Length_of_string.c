4ba85b82e4f10a1e052f0eef957ab4442686cc95
Komal Sharma <75818108+komal7141@users.noreply.github.com>
2021-02-26 11:59:56 +0530

    Update_length_of_string.c

--- Length_of_string.c
+++ Length_of_string.c
@@ -15,5 +15,6 @@ int main()
     return 0;
 }
     
-    
+/* Here using while loop we have iterated over characters of string from p=str(which assigned to the base address of string) to until '\0'(null character is encountered in each iteration value of p is increase by 1.
+When the loop end the length of string will be stored in the p variable.*/    
     
