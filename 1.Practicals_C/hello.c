3197096ab023a54bd8e634b21735198073e4bca5
Sarvesh <pandeysarvesh100@gmail.com>
2022-03-25 15:03:27 +0530

    my first commit

---
+++ 1.Practicals_C/hello.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <conio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+   getch();
+
+}
+
+// #include <stdio.h>
+// int main() {    
+
+//     int number1, number2, sum;
+    
+//     printf("Enter two integers: ");
+//     scanf("%d %d", &number1, &number2);
+
+//     // calculating sum
+//     sum = number1 + number2;      
+    
+//     printf("%d + %d = %d", number1, number2, sum);
+//     return 0;
+// }
+
+// To run the program from command line instead of doing so using code runner support.
+
+// gcc hello.c -o hello
+// .\hello
