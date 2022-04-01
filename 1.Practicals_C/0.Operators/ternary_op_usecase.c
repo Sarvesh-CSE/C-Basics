dcb31bf3ecc900cd9344b5f2cb01ba239ccc387a
Sarvesh <pandeysarvesh100@gmail.com>
2022-04-01 14:56:33 +0530

    commented ternary operator code

--- 1.Practicals_C/0.Operators/ternary_op_usecase.c
+++ 1.Practicals_C/0.Operators/ternary_op_usecase.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int y;
+    int y=0;
     int x = 2;
+    // if (x >= 6)
+    //     printf("6");
+    // else{
+    //     y=x;
+    //     printf("y= %d \n", x);}
     y = (x >= 6) ?  6 : x; /* This is equivalent to:  if (x >= 6)    y = 6;  else    y = x; */
+    // expression ? this_if_true : this_if_false
     printf("y =%d ",y);
     return 0;
 }
